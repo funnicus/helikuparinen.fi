112fd3d7b58d055dd023b0a2a995b1409762c5e7
funnicus <juhku@elisanet.fi>
2021-06-08 23:08:58 +0300

    #patch

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -4,6 +4,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { getContent } from '@services/contentful';
 
 export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+    //1
     const response = await getContent('fi-FI', 'post');
     console.log(response);
     res.status(200).json(response);
