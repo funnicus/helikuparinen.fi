bf603780e22105f9d76cefc74c0faf5d832d6800
funnicus <juhku@elisanet.fi>
2021-06-14 01:22:15 +0300

    added analytics

---
+++ src/helpers/gtag.ts
@@ -0,0 +1,19 @@
+export const GA_TRACKING_ID = 'G-S6P4JWRS8Y'; // This is your GA Tracking ID
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+export const pageview = (url) => {
+    window.gtag('config', GA_TRACKING_ID, {
+        page_path: url,
+    });
+};
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+export const event = ({ action, category, label, value }) => {
+    if (typeof window !== 'undefined') {
+        window.gtag('event', action, {
+            event_category: category,
+            event_label: label,
+            value: value,
+        });
+    }
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import { AppProps } from 'next/app';
+import Router from 'next/router';
 
 import Layout from '@components/layout';
+import * as gtag from '@helpers/gtag';
 import { reducer, StateProvider } from '@state/index';
 
 import '@styles/globals.css';
 import '@styles/about.css';
+
+//we track pageview when route is changed
+Router.events.on('routeChangeComplete', (url) => gtag.pageview(url));
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     return (
