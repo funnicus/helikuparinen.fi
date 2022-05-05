055c6fb4ad8d9e22d13851b7e0ed3c326829c383
Juhana Kuparinen <juhku@elisanet.fi>
2022-05-05 13:44:18 +0300

    preupdate

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,43 +1,46 @@
-import { Fragment } from 'react';
-import Document, { 
-    Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps 
-} from 'next/document';
+import { Fragment } from "react";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from "next/document";
 
-import { GA_TRACKING_ID } from '@helpers/gtag';
+import { GA_TRACKING_ID } from "@helpers/gtag";
 
 class MyDocument extends Document<Props> {
+  static async getInitialProps(ctx: DocumentContext): Promise<Props> {
+    const initialProps = await Document.getInitialProps(ctx);
 
-    static async getInitialProps(ctx: DocumentContext): Promise<Props> {
-        const initialProps = await Document.getInitialProps(ctx);
+    // Check if in production
+    const isProduction = process.env.NODE_ENV === "production";
 
-        // Check if in production
-        const isProduction = process.env.NODE_ENV === 'production';
+    return {
+      ...initialProps,
+      isProduction,
+    };
+  }
 
-        return {
-            ...initialProps,
-            isProduction,
-        };
-    }
+  render(): JSX.Element {
+    const { isProduction } = this.props;
 
-    render(): JSX.Element {
+    return (
+      <Html>
+        <Head />
 
-        const { isProduction } = this.props;
-
-        return (
-            <Html>
-                <Head />
-
-                {/* We only want to add the scripts if in production */}
-                {isProduction && (
-                    <Fragment>
-                        {/* Global Site Tag (gtag.js) - Google Analytics */}
-                        <script
-                            async
-                            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-                        />
-                        <script
-                            dangerouslySetInnerHTML={{
-                                __html: `
+        {/* We only want to add the scripts if in production */}
+        {isProduction && (
+          <Fragment>
+            {/* Global Site Tag (gtag.js) - Google Analytics */}
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+            />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
                                         window.dataLayer = window.dataLayer || [];
                                         function gtag(){ dataLayer.push(arguments); }
                                         gtag('js', new Date());
@@ -46,22 +49,22 @@ class MyDocument extends Document<Props> {
                                             page_path: window.location.pathname,
                                         });
                                     `,
-                            }}
-                        />
-                    </Fragment>
-                )}
+              }}
+            />
+          </Fragment>
+        )}
 
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        );
-    }
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
 interface Props extends DocumentInitialProps {
-    isProduction?: boolean;
+  isProduction?: boolean;
 }
 
 export default MyDocument;
